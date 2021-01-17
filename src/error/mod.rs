e1a4340655ae26ec5d632e5f51a2bed8b0e766e6
Osamu Koga <osak.63@gmail.com>
2021-01-17 23:37:01 +0000

    sample code

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub struct FlamError {
     pub(crate) source: Box<dyn std::error::Error>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 mod client;
 mod error;
 
-fn main() {
-    println!("Hello, world!");
+use reqwest::Url;
+use client::html::{Client, Config};
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new(&Config::default()).unwrap();
+    let url = Url::parse("https://osak.jp").unwrap();
+    let result = client.get(url).await.unwrap();
+    println!("{}", result);
 }
